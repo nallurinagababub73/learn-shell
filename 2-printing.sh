27acbf279301117180c5eabf1e1c425114c83f1c
nagababu <nag4c1@gmail.com>
2023-05-26 16:48:34 +0530

    first commit

--- 2-printing.sh
+++ 2-printing.sh
@@ -1,1 +1,10 @@
 echo hello devops
+
+# Printing some colours
+
+echo -e "\e[31m Hello devops in red\e[0m"
+echo -e "\e[32m Hello devops in green \e[0m"
+echo -e " \e[33m Hello devops in yellow \e[0m"
+echo -e "\e[34m Hello devops in blue \e[0m"
+echo -e " \e[35m Hello devops in magenta \e[0m"
+echo -e " \e[36m Hello devops in cyan \e[0m"
